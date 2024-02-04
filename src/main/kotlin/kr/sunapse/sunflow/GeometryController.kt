acf086cb31141818bcbf522ab5c6dce6ba55ae97
shinjayne <shinjayne@gmail.com>
2024-02-04 23:28:44 +0900

    add health check path

--- src/main/kotlin/kr/sunapse/sunflow/GeometryController.kt
+++ src/main/kotlin/kr/sunapse/sunflow/GeometryController.kt
@@ -1,5 +1,6 @@
 package kr.sunapse.sunflow
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -7,6 +8,11 @@ import kotlin.math.round
 
 @RestController
 class GeometryController(private val geometryService: GeometryService) {
+
+    @GetMapping("/", "/hello")
+    fun heathCheck(): Boolean {
+        return true
+    }
 
     @PostMapping("/geometry/search")
     fun getGeometry(@RequestBody request: GeometrySearchBody): GeometryResponse {
